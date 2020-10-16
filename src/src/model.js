bd9724a8120bb22d076fb47c455e3ef26805f000
emmiii <frisk.emma@hotmail.com>
2020-10-16 21:03:43 +0200

    api requests working and returned to view

--- src/src/model.js
+++ src/src/model.js
@@ -1,4 +1,4 @@
-
+const baseUrl = 'https://api.github.com'
 
 export default class Model {
 
@@ -6,5 +6,12 @@ export default class Model {
         console.log('model created')
     }
 
-    
+    async getGithubUser(username) {
+        const respons = await fetch(`${baseUrl}/users/${username}`)
+        const user = await respons.json()
+        console.log(respons.headers.get('x-ratelimit-remaining'))
+        return user
+
+    }
+
 }
--- src/src/view.js
+++ src/src/view.js
@@ -1,10 +1,15 @@
 
-
+const main = document.querySelector('main')
 export default class View {
 
     constructor() {
         console.log('view created')
     }
 
+    showUser(user) {
+        debugger
+        main.textContent = JSON.stringify(user)
+
+    }
     
 }
