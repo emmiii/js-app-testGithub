fa30f5670d2a3f19ec251925abd862225b02b2e7
emmiii <frisk.emma@hotmail.com>
2020-10-16 20:28:12 +0200

    inital app setup

---
+++ src/src/controller.js
@@ -0,0 +1,16 @@
+const searchButton = document.querySelector('#search-button')
+
+export default class Controller {
+
+    constructor(model, view) {
+        this.model = model
+        this.view = view
+        console.log('controller created')
+        searchButton.addEventListener('click', this.search)
+    }
+
+    search() {
+        console.log('user clicked search')
+    }
+    
+}
